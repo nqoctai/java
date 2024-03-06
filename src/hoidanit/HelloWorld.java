8545046288a73af9a8e37577a8f65cfa57811d9a
nqoctai <jeweltai1210@gmail.com>
2024-03-06 16:03:03 +0700

    init project

---
+++ src/hoidanit/HelloWorld.java
@@ -0,0 +1,9 @@
+package hoidanit;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with Java and Hoi Dan IT");
+		System.out.println("Hello World with Java and Hoi Dan IT");
+		System.out.println("Hello World with Java and Hoi Dan IT");
+	}
+}
